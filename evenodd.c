17147df39d0c50a4845376bc1185495e61735f5e
sakshi-7662 <sakshishinde0671@gmail.com>
2025-06-27 18:30:09 +0530

    update

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool isEven(int number) {
+    return number % 2 == 0;
+}
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    bool result = isEven(number);
+    if (result) {
+        printf("true\n"); 
+    } else {
+        printf("false\n"); 
+    }
+}
